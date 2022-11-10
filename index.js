14bf229b9ba12d9bf68d5913e5b45793feccaa31
RaiAn-CSE <robiul-1512392@diu.edu.bd>
2022-11-10 19:56:46 +0600

    update request added

--- index.js
+++ index.js
@@ -22,16 +22,16 @@ async function run() {
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
-            const services = await cursor.toArray();
+            const services = await cursor.limit(3).toArray();
             res.send(services);
         });
 
-        // app.get('/services', async (req, res) => {
-        //     const query = {}
-        //     const cursor = serviceCollection.find(query);
-        //     const services = await cursor.limit(3).toArray();
-        //     res.send(services);
-        // });
+        app.get('/allservices', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
 
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
