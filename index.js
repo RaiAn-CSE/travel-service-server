47f033ca3da8ff9bd78cd03574c79937ed2e6864
RaiAn-CSE <robiul-1512392@diu.edu.bd>
2022-11-10 17:45:22 +0600

    review data delete option added

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,9 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3exxtfz.mongodb.net/?retryWrites=true&w=majority`;
-
 // console.log(uri);
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
@@ -28,6 +25,13 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
         });
+
+        // app.get('/services', async (req, res) => {
+        //     const query = {}
+        //     const cursor = serviceCollection.find(query);
+        //     const services = await cursor.limit(3).toArray();
+        //     res.send(services);
+        // });
 
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
@@ -44,7 +48,6 @@ async function run() {
             const result = await serviceCollection.insertOne(user)
             res.send(result)
         })
-        // reviews
         app.get('/reviews', async (req, res) => {
             const query = {}
             const cursor = reviewCollection.find(query)
